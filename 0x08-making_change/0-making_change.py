8058b0ed56613d1e45511371316317d0e6a18e00
Michael Olaniyan <103427098+mikolanny@users.noreply.github.com>
2024-06-20 20:53:09 +0100

    fixed req. error

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python3
-"""A function to determine the fewest number of coins needed
-   to meet a given amount total"""
+"""
+Coin Change Algorithm
+"""
 
 
 def makeChange(coins, total):
-    """This function will take a list of coins and use
-       that to calculate how much change the total will require
+    """Calculate the fewest number needed to meet,
+    needed to meet a given total amount.
+    Args:
+        coins ([list]): A list of coin values available.
+        total ([number]): The target amount
+    Return: The fewest number of coins needed to reach the total,
+    or -1 if not possible.
     """
     if total <= 0:
         return 0
 
-    else:
-        coin = sorted(coins)
-        coin.reverse()
-        counter = 0
-        for i in coin:
-            while(total >= i):
-                counter += 1
-                total -= i
-        if total == 0:
-            return counter
-        return -1
+    coins.sort(reverse=True)
 
+    i, ncoins = (0, 0)
+    cpy_total = total
+    len_coins = len(coins)
+
+    while(i < len_coins and cpy_total > 0):
+        if (cpy_total - coins[i]) >= 0:
+            cpy_total -= coins[i]
+            ncoins += 1
+        else:
+            i += 1
+
+    check = cpy_total > 0 and ncoins > 0
+    return -1 if check or ncoins == 0 else ncoins
